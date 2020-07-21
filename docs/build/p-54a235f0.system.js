a3d2da5b7f1f0e36aa312fb995eeec8ec01fe0f8
jared b <jaredcbaum@gmail.com>
2020-07-20 23:12:54 -0500

    bumped site shared version for platform-bar changes

--- docs/build/p-54a235f0.system.js
+++
@@ -1,1 +0,0 @@
-System.register(["./p-22e300c7.system.js","./p-b18c450e.system.js","./p-bb508a3b.system.js"],(function(){"use strict";return{setters:[function(){},function(){},function(){}],execute:function(){}}}));
