0205e9acee436000a976de33e3458ef62388818e
Marlon Harrison <marlon@ionic.io>
2022-06-07 14:04:59 -0500

    fix(landing): remove reference to web font support

--- src/components/landing-page/landing-page.tsx
+++ src/components/landing-page/landing-page.tsx
@@ -18,7 +18,7 @@ export class LandingPage {
           <ResponsiveContainer>
             <div class="content">
               <h1>Open source icons.<br /><span>Lovingly hand-crafted.</span></h1>
-              <p class="lead">Premium designed icons for use in web, iOS, Android, and desktop apps. Support for SVG and web font. Completely open source, MIT licensed and built by <a href="https://ionic.io">Ionic</a>.</p>
+              <p class="lead">Premium designed icons for use in web, iOS, Android, and desktop apps. Support for SVG. Completely open source, MIT licensed and built by <a href="https://ionic.io">Ionic</a>.</p>
             </div>
           </ResponsiveContainer>
           <icon-list query={this.query} data={this.data}></icon-list>
