ff2dad02f387c6e04486ba86bff08cdc90b3a2d9
Brandy Carney <brandy@ionic.io>
2023-03-03 15:38:41 -0500

    fix(notfound): style fixes

--- src/components/notfound-page/notfound-page.tsx
+++ src/components/notfound-page/notfound-page.tsx
@@ -12,7 +12,7 @@ export class NotFoundPage {
       <main>
         <ResponsiveContainer>
           <h1>Oops! We can't find the page you're looking for.</h1>
-          <p>Head on back to the <stencil-route-link url="/ionicons" exact={true} class="block">Icons page</stencil-route-link>.</p>
+          <p>Head back to the <stencil-route-link url="/ionicons" exact={true} class="block">Icons</stencil-route-link>.</p>
         </ResponsiveContainer>
 
         <footer-bar></footer-bar>
