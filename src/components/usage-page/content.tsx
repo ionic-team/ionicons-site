47ad88b7b13c27faaeafb6cf5c816a90e14ff558
Shawn Taylor <shawn@ionic.io>
2023-09-18 13:10:30 -0400

    Update content.tsx

--- src/components/usage-page/content.tsx
+++ src/components/usage-page/content.tsx
@@ -157,7 +157,6 @@ export default function (version: string, name = "heart", suffix = "") {
       )}
 
       <h2>Browser support</h2>
-      <p>Ionicons are supported on the browsers listed below:</p>
       <table>
         <caption>Supported browsers</caption>
         <thead>
@@ -199,6 +198,7 @@ export default function (version: string, name = "heart", suffix = "") {
           </tr>
         </tbody>
       </table>
+      <p>Report problems with this compatibility data on <a href="https://github.com/ionic-team/ionicons/issues" target="_blank">GitHub</a></p>
 
       <h2>Migrating from v4</h2>
       <p>
